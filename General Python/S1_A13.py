cfe6243bb56821533f9aa5e7ee876a165c369d2f
Luis <garay_luis@ymail.com>
2017-11-02 13:48:23 -0500

    Completed S1_A13 and S1_A14 (w/ a dictionary and w/o a dictionary)

--- General Python/S1_A13.py
+++ General Python/S1_A13.py
@@ -1,19 +1,28 @@
-#Imports
+#Import
 import socket
 
-#Code
+#Vars
 hostname = "google.com"
 port = 80
-sock = socket.socket()
 
-#Remote
-print("Remote IP Address:" + socket.gethostbyname(hostname))
-print("Remote Port Address:")
+#Connection
+try:
+    sock = socket.socket()
+    sock.connect((hostname, port))
+except Exception as e:
+    print(e)
+    quit()
 
-#Local
-print("Local IP Address:")
-print("Local Port Address:")
+#Output
+print("Remote IP Address: " + str(sock.getpeername()[0]))
+print("Remote Port Address: " + str(sock.getpeername()[1]))
+print("Local IP Address: " + str(sock.getsockname()[0]))
+print("Local Port Address: " + str(sock.getsockname()[1]))
 
-
-#To see if port is open
-result = sock.connect((hostname, port))
+#Notes
+#socket.getpeername() returns an array with remote socket information:
+#[0] = Remote IP Address
+#[1] = Remote Port Address
+#socket.getsockname() returns an array with local socket information:
+#[0] = Local IP Address
+#[1] = Local Port Address
