fc825511ad858fe7e645588777ded47176222fb5
Luis <garayluis568@gmail.com>
2017-11-28 20:16:15 -0600

    Testing ASP.NET
    - Using .NET Framework 4.0 due to IDE constraints

---
+++ Website/ASP.NET/WebSites/RemoteSite/Login.aspx.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public partial class Login : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+
+    }
+}
---
+++ Website/ASP.NET/WebSites/RemoteSite/clients/Clients.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public partial class clients_Clients : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+
+    }
+
+    private double celsius2Fahrenheit(double celsius) {
+        return ((9 / 5) * celsius) + 32;
+    }
+
+    private double fahrenheit2Celsius(double fahrenheit) {
+        return (5 / 9) * (fahrenheit - 32);
+    }
+}
