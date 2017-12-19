d0e1a5743f2723fb28b69d7a24a6c59711f9b68a
luis <garay_luis@ymail.com>
2017-12-18 19:39:41 -0600

    Changed Buffered Reader/Writer from String to Bytes, XML from python client is created on Java Server, about test SQL query after receiving, creating, and reading XML

--- Masters Project/Client/client.py
+++ Masters Project/Client/client.py
@@ -1,20 +1,35 @@
 #import
+import os
+import time
 import socket
+from datetime import datetime
+from xml.etree import ElementTree
+
+#------------------------------TODO: Read from sensors and put data into XML-----------------------------------#
 
 #Get File
 fileName = 'status.xml'
-xmlFile = open(fileName, 'r')
+filePath = os.path.abspath(fileName)
 
+#Parse XML
+xmlParsed = ElementTree.parse(filePath)
+xmlRoot = xmlParsed.getroot()
+
+#Update XML's log to current timestamp
+xmlLogElement = xmlRoot.find('log')
+xmlLogElement.text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+xmlParsed.write('status.xml')
+
+'''
 #Create String to Send to Server
 xml = []
 for line in xmlFile:
 	xml.append(line)
 xmlString = ''.join(xml)
-#print(xmlString)
-
+print(xmlString)
+'''
 
 #--------------------------------------------------------------------------------------------------------------#
-
 
 #Socket Connection Init
 servername = '127.0.0.1'
@@ -34,12 +49,19 @@ try:
 		if request.upper() == 'QUIT':
 			break
 		elif request.upper() == 'XML':
-			client.sendall(bytes(xmlString + '\n', 'utf-8'))
+			print("Preparing to send XML to Server")
+			xmlFile = open(filePath, 'rb')
+			xmlBytes = xmlFile.read(1024)
+			time.sleep(5)
+			while(xmlBytes):
+				client.send(xmlBytes)
+				xmlBytes = xmlFile.read(1024)
+			xmlFile.close()
+			print("Done Sending")
 		
 		#Print Received Data
 		received = str(client.recv(1024), 'utf-8')
 		print("{}".format(received))
-	
-finally: #Finally, just close connection
+finally:
 	print('Closing Connection...')
 	client.close()
