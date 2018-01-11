287d536d3126b548a89b179af8cde16548fa1761
lsg_pi <garay_luis@ymail.com>
2018-01-11 16:59:38 -0600

    Edited python script

--- MP/RaspberryPi/client.py
+++ MP/RaspberryPi/client.py
@@ -32,24 +32,30 @@ print(xmlString)
 #--------------------------------------------------------------------------------------------------------------#
 
 #Socket Connection Init
-servername = '127.0.0.1'
+servername = '192.168.2.29'
 port = 8080
 client = socket.socket()
+credentialsVerified = False;
 
 #Client/Server Communication
 try:
 	client.connect((servername, port)) #Connect
 	
 	while True:
-		#End Program or Continue Requesting Data
-		request = input("What would you like to request from the server? - 'Quit' closes the connection.\n")
+            client.sendall(bytes('signin\n' + '\n' + '\n'))
+            if str(client.recv(1024), 'utf-8').upper() == 'ACCEPT':
+                print('{}'.format('Login Successful!'))
+                credentialsVerified = True;
+            if credentialsVerified:
+                #End Program or Continue Requesting Data
+		request = input('What would you like to request from the server? - 'Quit' closes the connection.\n')
 		client.sendall(bytes(request + '\n', 'utf-8'))
 		
 		#Process Received Data
 		if request.upper() == 'QUIT':
 			break
 		elif request.upper() == 'XML':
-			print("Preparing to send XML to Server")
+			print('Preparing to send XML to Server')
 			xmlFile = open(filePath, 'rb')
 			xmlBytes = xmlFile.read(1024)
 			time.sleep(5)
@@ -57,11 +63,11 @@ try:
 				client.send(xmlBytes)
 				xmlBytes = xmlFile.read(1024)
 			xmlFile.close()
-			print("Done Sending")
+			print('Done Sending')
 		
 		#Print Received Data
 		received = str(client.recv(1024), 'utf-8')
-		print("{}".format(received))
+		print('{}'.format(received))
 finally:
 	print('Closing Connection...')
 	client.close()
