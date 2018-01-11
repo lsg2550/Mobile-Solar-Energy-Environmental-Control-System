97ebc46b27ef5c74c1446a84f4a59bd4ac4742ef
lsg_pi <garay_luis@ymail.com>
2018-01-11 17:58:06 -0600

    Raspberry Pi sends credentials to the server for verification

--- MP/RaspberryPi/client.py
+++ MP/RaspberryPi/client.py
@@ -35,39 +35,42 @@ print(xmlString)
 servername = '192.168.2.29'
 port = 8080
 client = socket.socket()
-credentialsVerified = False;
+credentialsVerified = False
 
 #Client/Server Communication
 try:
-	client.connect((servername, port)) #Connect
+    client.connect((servername, port)) #Connect
 	
-	while True:
-            client.sendall(bytes('signin\n' + '\n' + '\n'))
-            if str(client.recv(1024), 'utf-8').upper() == 'ACCEPT':
-                print('{}'.format('Login Successful!'))
-                credentialsVerified = True;
+    while True:
+        #Sign In - signin\username\password
+        client.sendall(bytes('signin\n' + '\n' + '\n', 'utf-8'))
+        
+        if str(client.recv(1024), 'utf-8').upper() == 'ACCEPT':
+            print('{}'.format('Login Successful!'))
+            credentialsVerified = True
+            
             if credentialsVerified:
                 #End Program or Continue Requesting Data
-		request = input('What would you like to request from the server? - 'Quit' closes the connection.\n')
-		client.sendall(bytes(request + '\n', 'utf-8'))
+                request = input('What would you like to request from the server? - "Quit" closes the connection.\n')
+                client.sendall(bytes(request + '\n', 'utf-8'))
 		
 		#Process Received Data
-		if request.upper() == 'QUIT':
-			break
-		elif request.upper() == 'XML':
-			print('Preparing to send XML to Server')
-			xmlFile = open(filePath, 'rb')
-			xmlBytes = xmlFile.read(1024)
-			time.sleep(5)
-			while(xmlBytes):
-				client.send(xmlBytes)
-				xmlBytes = xmlFile.read(1024)
-			xmlFile.close()
-			print('Done Sending')
-		
+                if request.upper() == 'QUIT':
+                    break
+                elif request.upper() == 'XML':
+                    print('Preparing to send XML to Server')
+                    xmlFile = open(filePath, 'rb')
+                    xmlBytes = xmlFile.read(1024)
+                    time.sleep(5)
+                    while(xmlBytes):
+                        client.send(xmlBytes)
+                        xmlBytes = xmlFile.read(1024)
+                    xmlFile.close()
+                    print('Done Sending')
+
 		#Print Received Data
-		received = str(client.recv(1024), 'utf-8')
-		print('{}'.format(received))
+                received = str(client.recv(1024), 'utf-8')
+                print('{}'.format(received))
 finally:
-	print('Closing Connection...')
-	client.close()
+    print('Closing Connection...')
+    client.close()
