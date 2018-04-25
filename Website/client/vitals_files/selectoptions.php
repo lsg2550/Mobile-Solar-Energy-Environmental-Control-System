cc948a25c328cf702b239bce1c56f7b9cecbb93b
Luis <garay_luis@ymail.com>
2018-04-25 15:43:55 -0500

    Made some changes to vitalsthreshold and selectoptions, will have to go back and rework selectoptions to accomodate multiple forms

--- Website/client/vitals_files/selectoptions.php
+++ Website/client/vitals_files/selectoptions.php
@@ -1,6 +1,5 @@
 <?php
-
-//This will only be called for the vitals that are contrallable (overwritable) - e.g Solar Panel, Exhaust
+//This will only be called for the vitals that are controllable (overwritable) - e.g Solar Panel, Exhaust
 function generateSelectOptions($vitalname, $currentstatus, $rpid) {
     $selectHTML = '<th>' . $vitalname . '</th>';
     
@@ -25,21 +24,28 @@ function generateSelectOptions($vitalname, $currentstatus, $rpid) {
     $selectHTML .= '<th><select>'; //Second Column - Status Column
     $selectHTML .= '<option name="currentstatus[]" value="' . $currentstatus . '">' . $currentstatus . '</option>';
     $selectHTML .= '<option name="secondOption[]" value="' . $secondOption . '"> ' . $secondOption . ' </option>';
-    $selectHTML .= '</select> <input type="hidden" name="rpid[]" value="' . $rpid . '"> </th>';
+    $selectHTML .= '</select></th>';
+    $selectHTML .= '<input type="hidden" name="vitalname[]" value="' . $vitalname . '">';
+    $selectHTML .= '<input type="hidden" name="rpid[]" value="' . $rpid . '">';
     return $selectHTML;
 }
 
-//This will only be called for the vitals that are not contrallable (overwritable), but whose thresholds are - e.g Battery, Temperature, Photo
+//This will only be called for the vitals that are not controllable (overwritable), but whose thresholds are - e.g Battery, Temperature, Photo
 function generateThresholdOptions($vitalname, $vitallower, $vitalupper, $rpid) {
-    $selectHTML = '<th>' . $vitalname . '</th>';
+    $selectHTML = '';
+    
+    //Populate threshold row START
+    $selectHTML .= '<th>' . $vitalname . '</th>';
     $selectHTML .= '<th>'; //Second Column - VLower Column
     $selectHTML .= '<input type="text" name="vitallower[]" value="' . $vitallower . '">';
     $selectHTML .= '</th>';
     $selectHTML .= '<th>'; //Third Column - VUpper Column
     $selectHTML .= '<input type="text" name="vitalupper[]" value="' . $vitalupper . '">';
     $selectHTML .= '</th>';
+    $selectHTML .= '<input type="hidden" name="vitalname[]" value="' . $vitalname . '">';
     $selectHTML .= '<input type="hidden" name="rpid[]" value="' . $rpid . '">';
+    //Populate threshold row END
+    
     return $selectHTML;
 } 
-
 ?>
--- Website/client/vitals_files/vitalsthreshold.php
+++ Website/client/vitals_files/vitalsthreshold.php
@@ -1,16 +1,20 @@
 <?php
-    echo 'LOWER:';
-    foreach($_POST['vitallower'] as $vital) {
-        echo $vital . '<br>';
-    }
+    //TODO: Filter $_POST 
+    //This php code will update the 'vitals' table for the respective raspberry pi and its user
+    $currentUser = $_SESSION['username']; //Get Current User Name
+    $amtOfRPis = count(array_unique($_POST['rpid'])); //Get the amount of RPis to edit
+    $amtOfVitals = count($_POST['vitallower']); //Get the amount of Vitals to edit - will be the same count for $vitalupper and $vitalname
+    $sqlUpdate = array();
 
-    echo 'UPPER:';
-    foreach($_POST['vitalupper'] as $vital) {
-        echo $vital . '<br>';
-    }
-
-    echo 'RPID:';
-    foreach($_POST['rpid'] as $vital) {
-        echo $vital . '<br>';
+    for($amtOfRPis - 1; $amtOfRPis >= 0; $amtOfRPis--) {
+        $tempRPi = $_POST['rpid'][$amtOfRPis];
+        echo 'RPID:' . $tempRPi;
+        for($amtOfVitals - 1; $amtOfVitals >= 0; $amtOfVitals--) {
+            $tempVL = $_POST['vitallower'][$amtOfVitals];
+            $tempVU = $_POST['vitalupper'][$amtOfVitals];
+            echo 'LOWER:' . tempVL;
+            echo 'UPPER:' . tempVU;
+            $sqlUpdate[] = 'UPDATE vitals SET VL="' . $tempVL . '", VU="' . $tempVU . '" WHERE RPID= "' . $tempRPI . '" AND USR="' . $currentUser . '"'; //TODO: ADD VITALNAME
+        }
     }
 ?>
