c452e9cdfd3dafebbc4646dc38fbb45885510eab
Luis <garayluis568@gmail.com>
2018-05-03 14:42:40 -0500

    Update
    Worked a little bit on App before VS2015 decided to break.

--- Mobile App/RemoteSite/RemoteSite/Resources/Resource.Designer.cs
+++ Mobile App/RemoteSite/RemoteSite/Resources/Resource.Designer.cs
@@ -61,7 +61,31 @@ namespace RemoteSite
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int webView = 2131034112;
+			public const int LinearLayoutRoot = 2131034112;
+			
+			// aapt resource value: 0x7f050008
+			public const int buttonSignIn = 2131034120;
+			
+			// aapt resource value: 0x7f050007
+			public const int relativeLayoutButton = 2131034119;
+			
+			// aapt resource value: 0x7f050003
+			public const int space_1 = 2131034115;
+			
+			// aapt resource value: 0x7f050006
+			public const int space_2 = 2131034118;
+			
+			// aapt resource value: 0x7f050005
+			public const int textField_Password = 2131034117;
+			
+			// aapt resource value: 0x7f050002
+			public const int textField_Username = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView_Password = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int textView_Username = 2131034113;
 			
 			static Id()
 			{
