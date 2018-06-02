e78d1345503151e5a7a29cdd75f620acb4ad5231
Luis <luis.garay01@utrgv.edu>
2018-06-01 23:34:26 -0500

    Update

--- Raspberry Pi/generate_send_xml.py
+++ Raspberry Pi/generate_send_xml.py
@@ -8,56 +8,72 @@
 import os
 import time
 from xml.etree import ElementTree
-from connect_to_ftp import sendXML
+from connect_to_ftp import SendXML
+from read_analog_from_adc import ReadFromSensors
 from datetime import datetime
 from pytz import timezone
 
 #Init
-tempXML = "<currentstatus><log></log><temperature>20</temperature><battery>12.6</battery><solarpanel>not charging</solarpanel><exhaust>off</exhaust><photo>0.5</photo><rpid></rpid></currentstatus>"
+xmlStorageDirectory = "xmlstorage"
+try:
+    os.mkdir(xmlStorageDirectory)
+except:
+    pass
+tempXML = "<currentstatus><log></log><temperature>20</temperature><battery></battery><solarpanel></solarpanel><solarpanelvalue></solarpanelvalue><exhaust>off</exhaust><photo>0.5</photo><rpid></rpid></currentstatus>"
 startTime = time.time()
 dateFormat = "%Y-%m-%d %H:%M:%S"
 
 while True:
-    #Read from sensors and put data into XML
-    
-
     #Timestamps
     currentTimeStampForLog = datetime.now(timezone("UTC")).strftime(dateFormat)
     currentTimeStampForFileName = currentTimeStampForLog.replace(":", "-")
-
+    
     #Create File
     fileName = "status(" + currentTimeStampForFileName + ").xml"
     fileXML = open(fileName, "w+")
     fileXML.write(tempXML) #TODO: Replace this with looped code to fill XML with data from sensors
     fileXML.close()
-
-    #Parse XML & Update XML's log to current timestamp
+    
+    #Read from sensors & Parse XML & Update XML's log to current timestamp
     fileXML = open(fileName, "r+")
     xmlParsed = ElementTree.parse(fileXML)
+    xmlRoot = xmlParsed.getroot()
     fileXML.close()
     fileXML = open(fileName, "w").close()
+    fileXML = open(fileName, "r+")
 
-    xmlRoot = xmlParsed.getroot()
+    sensorDictionary = ReadFromSensors()
+    for key, value in sensorDictionary.items():
+        xmlElement = xmlRoot.find(key)
+        xmlElement.text = str(value)
+    #foreach end 
     xmlLogElement = xmlRoot.find("log")
     xmlLogElement.text = str(currentTimeStampForLog)
     xmlRpidElement = xmlRoot.find("rpid")
     xmlRpidElement.text = str(0) #This will be the raspberry pi's identification number
-
-    fileXML = open(fileName, "r+")
     xmlParsed.write(fileXML)
     fileXML.close()
 
     #Send XML to Server
     try:
         #Check 'temporary folder' for any unsent XML files, then send them and delete them, then continue on sending the most recent file (update)
-        sendXML(fileName)
-    except:
-        #Code to store XML into a 'temporary folder', and continue on with the program
+        #storedXMLFiles = []
+        files = os.listdir(xmlStorageDirectory)
+        for storedFile in files:
+            tempFile = str(storedFile)
+            if tempFile.endswith(".xml"):
+                #storedXMLFiles.append(tempFile)
+                print(tempFile)
+                SendXML(xmlStorageDirectory + "/" + tempFile)
+        SendXML(fileName)
+    except Exception as e:
+        #print(e)
+        os.rename(fileName, xmlStorageDirectory + "/" + fileName) #Move File to Temporary Storage Folder
         print("Could not connect to server...")
         print("Storing XML into temporary folder...")
     finally:
         #Wait for 60 seconds
         timer = (time.time() - startTime) % 60
-        print("Standby for (60.0 - {}) = {} seconds".format(str(timer), str((60.0 - timer))))
+        print("Standby for {0:.2} seconds".format(str((60.0 - timer))))
         time.sleep(60.0 - timer)
 #while end
--- Raspberry Pi/read_analog_from_adc.py
+++ Raspberry Pi/read_analog_from_adc.py
@@ -3,19 +3,6 @@ import spidev
 import time
 import os
 
-def ReadChannel(channel): #Reads from given channel
-    adc = spi.xfer2([1, (8 + channel) << 4,0])
-    data = ((adc[1] & 3) << 8) + adc[2]
-    return data
-def ConvertVolts(data, place): #Converts from read voltage to actual voltage (due to resisters
-    volts = (data * 3.3) / float(1023) #TODO: Change this
-    volts = round(volts, place)
-    return volts
-def CelciusToFahrenheit(temperatureCelcius):
-    return ((temperatureCelcius * 9/5) + 32)
-def FahrenheitToCelcius(temperatureFahrenheit):
-    return ((temperatureFahrenheit - 32) * 5/9)
-
 #Init
 delay = 1.0
 spi = spidev.SpiDev()
@@ -27,23 +14,50 @@ solar_panel = 1 #solar panel
 temperature = 2 #temperature sensor
 exhaust = 3 #Note: might delete this - as the exhaust might be handled by the cobbler
 
-#Read from Analog2Digital Converter
-while True:
+def ReadChannel(channel): #Reads from given channel
+    adc = spi.xfer2([1, (8 + channel) << 4,0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+def ConvertVolts(data, place): #Converts from read voltage to actual voltage (due to resisters
+    volts = (data * 3.3) / float(1023) #TODO: Change this
+    volts = round(volts, place)
+    return volts
+
+def CelciusToFahrenheit(temperatureCelcius):
+    return ((temperatureCelcius * 9/5) + 32)
+
+def FahrenheitToCelcius(temperatureFahrenheit):
+    return ((temperatureFahrenheit - 32) * 5/9)
+
+def ReadFromSensors():
+    #Dictionary to hold {Sensor => Value}
+    tempDictionary = {}
+    
     #Channel 0
     batteryReducedVoltage = ReadChannel(battery)
     batteryActualVoltage = ConvertVolts(batteryReducedVoltage, 2)
-    print("---------------------------------------------------------")
-    print("Battery Voltage: {} ({}V)".format(batteryReducedVoltage, batteryActualVoltage))
+    tempDictionary["battery"] = batteryActualVoltage
     
     #Channel 1
     solarPanelReducedVoltage = ReadChannel(solar_panel)
     solarPanelActualVoltage = ConvertVolts(solarPanelReducedVoltage, 2)
-    print("---------------------------------------------------------")
-    print("Solar Panel Voltage: {} ({}V)".format(solarPanelReducedVoltage, solarPanelActualVoltage))
+    tempDictionary["solarpanelvalue"] = solarPanelActualVoltage
+    if solarPanelActualVoltage >= 1:
+        tempDictionary["solarpanel"] = "charging"
+    else:
+        tempDictionary["solarpanel"] = "not charging"
     
     #Channel 2
     #temperatureValue = ReadChannel(temperature)
+    #tempDictionary["temperature"] = temperatureValue
+    
+    #Debug Output
+    #print("---------------------------------------------------------")
+    #print("Battery Voltage: {} ({}V)".format(batteryReducedVoltage, batteryActualVoltage))
+    #print("---------------------------------------------------------")
+    #print("Solar Panel Voltage: {} ({}V)".format(solarPanelReducedVoltage, solarPanelActualVoltage))
     #print("---------------------------------------------------------")
     #print("Temperature: {}C ({}F)".format(temperatureValue, CelciusToFahrenheit(temperatureValue)))
-
-    time.sleep(delay)
+    #print("")
+    return tempDictionary;
