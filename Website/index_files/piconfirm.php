bbead454507651373504204d3657f2ae622463b1
Luis <garayluis568@gmail.com>
2018-07-09 00:45:45 -0500

    Update

--- Website/index_files/piconfirm.php
+++ Website/index_files/piconfirm.php
@@ -1,12 +1,11 @@
 <?php
-function processXML($xmlFileName = null) {
+function processXML($xmlFileName) {
     //Include
     include("connect.php");
 
     //Init
     $xmldir = "../../xmls/";
     $processedxmldir = "../../processedxmls/";
-    $listOfXMLFiles = $xmlFileName; //Because we already confirmed that the file is a file and exists, we can safely place it into $listOfXMLFiles;
     $TYP = "ST";
 
     //Init - Get Username
@@ -15,47 +14,40 @@ function processXML($xmlFileName = null) {
     $USR = mysqli_fetch_assoc($resultsGetUser)['owner'];
 
     //Load xml files from $listOfXMLFiles & process them - grab data and update database
-    //sort($listOfXMLFiles);
-    foreach($listOfXMLFiles as $xmlFile) {
-        $xml = simplexml_load_file($xmldir . $xmlFile);
-        $RPID = $xml->rpid;
-        $TS = $xml->log;
-        //print_r($xml);
+    $xml = json_decode(file_get_contents($xmldir . $xmlFileName));
+    $RPID = $xml->{"rpid"};
+    $TS = $xml->{"log"};
 
-        foreach($xml as $key => $value) {
-            switch($key) {
-                case "log":
-                case "rpid":
-                case "solarpanelvalue":
-                    break;
-                default:
-                    //VitalName
-                    $VN = "";
-                    if($key === "solarpanel") { $VN = "solar panel"; } 
-                    else { $VN = $key; }
+    foreach($xml as $key => $value) {
+        switch($key) {
+            case "log":
+            case "rpid":
+            case "solarpanelvalue":
+                break;
+            default:
+                //VitalName
+                $VN = $key;
 
-                    //Get VID
-                    $sqlGetVID = "SELECT VID FROM vitals WHERE VN='{$VN}' AND RPID='{$RPID}' AND USR='{$USR}';";
-                    $resultGetVID = mysqli_query($conn, $sqlGetVID);
-                    $VID = mysqli_fetch_assoc($resultGetVID)['VID'];
+                //Get VID
+                $sqlGetVID = "SELECT VID FROM vitals WHERE VN='{$VN}' AND RPID='{$RPID}' AND USR='{$USR}';";
+                $resultGetVID = mysqli_query($conn, $sqlGetVID);
+                $VID = mysqli_fetch_assoc($resultGetVID)['VID'];
 
-                    //Vital Values
-                    $V1 = $VV = $value;
-                    $V2 = "";
+                //Vital Values
+                $V1 = $VV = $value;
+                $V2 = "";
 
-                    //Update DB
-                    $sqlInsertIntoLog = "INSERT INTO log (VID, TYP, USR, RPID, V1, V2, TS) VALUES ('{$VID}', '{$TYP}', '{$USR}', '{$RPID}', '{$V1}', '{$V2}', '{$TS}');";
-                    $sqlUpdateCurrentStatus = "UPDATE status SET VV='{$VV}', TS='{$TS}' WHERE VID='{$VID}' AND USR='{$USR}' AND RPID='{$RPID}';";
-                    $resultInsertIntoLog = mysqli_query($conn, $sqlInsertIntoLog);
-                    $resultUpdateCurrentStatus = mysqli_query($conn, $sqlUpdateCurrentStatus);
-                    //echo $sqlInsertIntoLog . "<br>" . $sqlUpdateCurrentStatus . "<br>";
-            }
+                //Update DB
+                $sqlInsertIntoLog = "INSERT INTO log (VID, TYP, USR, RPID, V1, V2, TS) VALUES ('{$VID}', '{$TYP}', '{$USR}', '{$RPID}', '{$V1}', '{$V2}', '{$TS}');";
+                $sqlUpdateCurrentStatus = "UPDATE status SET VV='{$VV}', TS='{$TS}' WHERE VID='{$VID}' AND USR='{$USR}' AND RPID='{$RPID}';";
+                $resultInsertIntoLog = mysqli_query($conn, $sqlInsertIntoLog);
+                $resultUpdateCurrentStatus = mysqli_query($conn, $sqlUpdateCurrentStatus);
+                //echo $sqlInsertIntoLog . "<br>" . $sqlUpdateCurrentStatus . "<br>";
         }
-
-        //Move out of the waiting xml folder
-        rename($xmldir . $xmlFile, $processedxmldir . $xmlFile);
     }
 
+    //Move out of the waiting xml folder
+    rename($xmldir . $xmlFileName, $processedxmldir . $xmlFileName);
     echo "OK";
 }
 
