e78439d70c91f17a6b9f368fcff2122ad5ca8b3d
luis <garay_luis@ymail.com>
2018-10-22 01:26:10 -0500

    Update Prepped code to read gps & cc data serially

--- Raspberry Pi/read_analog_from_adc.py
+++ Raspberry Pi/read_analog_from_adc.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import RPi.GPIO as GPIO
 import requests
 import spidev
+import serial
 import time
 import os
 import random
@@ -23,13 +24,44 @@ batteryVoltage = 0  # ESU - Voltage
 batteryCurrent = 1  # ESU - Current
 solarPanelVoltage = 2  # Solar panel voltage
 solarPanelCurrent = 3  # Solar panel current
-temperature = 4  # Temperature sensor
+temperatureInner = 4  # Temperature inner sensor
+temperatureOuter = 5  # Temperature outer sensor
 
 # GPIO Devices
 # exhaust = 22
 # engine = 23
 # GPIO.setup(exhaust. GPIO.OUT)
 # GPIO.setup(engine.GPIO.OUT)
+
+# Serial Devices
+# serialGPS = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600, timeout = 1)
+# serialChargeController = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600, timeout = 1)
+
+def ReadGPS():
+    tLatLon = {}
+    # line = serialGPS.readlines(10)
+    # data = line.split(",")
+    # if data[0] == "$GPRMC" and data[2] == "A": 
+    #     # Latitude
+    #     latGPS = float(data[3]) if data[4] != "S" else float(-data[3])
+    #     latDeg = int(latGPS/100)
+    #     latMin = latGPS - latDeg*100
+    #     latAct = latDeg + (latMin/60)
+
+    #     # Longitude
+    #     lonGPS = float(data[5]) if data[6] != "W" else float(-data[5])
+    #     lonDeg = int(lonGPS/100)
+    #     lonMin = lonGPS - lonDeg*100
+    #     lonAct = lonDeg + (lonMin/60)
+
+    #     tLatLon["latitude"] = latAct
+    #     tLatLon["longtitude"] = lonAct
+    return tLatLon
+
+def ReadChargeController():
+    tCVCCSVSC = {}
+    # line = serialChargeController.readlines(10)
+    return tCVCCSVSC
 
 def ReadChannel(channel):  # Reads from given channel
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
@@ -56,22 +88,34 @@ def ReadFromSensors(thresholdBattVoltageLower=None, thresholdBattVoltageUpper=No
     tempDictionary = {}
 
     # Channel 0 and 1 - Battery
-    #batteryVoltageReducedVoltage = ReadChannel(batteryVoltage)
-    #batteryVoltageActualVoltage = ConvertVolts(batteryVoltageReducedVoltage, 2)
-    #batteryCurrentRead = ReadChannel(batteryCurrent)
+    # batteryVoltageReducedVoltage = ReadChannel(batteryVoltage)
+    # batteryVoltageActualVoltage = ConvertVolts(batteryVoltageReducedVoltage, 2)
+    # batteryCurrentRead = ReadChannel(batteryCurrent)
     batteryVoltageActualVoltage = random.randint(11, 14)
     batteryCurrentRead = random.randint(0, 1000)
 
     # Channel 2 and 3 - Solar Panel
-    #solarPanelReducedVoltage = ReadChannel(solarPanelVoltage)
-    #solarPanelActualVoltage = ConvertVolts(solarPanelReducedVoltage, 2)
-    #solarPanelCurrentRead = ReadChannel(solarPanelCurrent)
+    # solarPanelReducedVoltage = ReadChannel(solarPanelVoltage)
+    # solarPanelActualVoltage = ConvertVolts(solarPanelReducedVoltage, 2)
+    # solarPanelCurrentRead = ReadChannel(solarPanelCurrent)
     solarPanelActualVoltage = random.randint(0, 17)
     solarPanelCurrentRead = random.randint(0, 1000)
 
-    # Channel 4 - Temperature
-    # temperatureValue = ReadChannel(temperature) #Celcius
-    temperatureValue = random.randint(0, 100)
+    # Channel 4 and 5 - Inner and Outer Temperature Sensors
+    # temperatureValueI = ReadChannel(temperatureInner) 
+    # temperatureValueO = ReadChannel(temperatureOuter) 
+    temperatureValueI = random.randint(0, 100)
+    temperatureValueO = random.randint(0, 100)
+
+    # Read Serially - GPS and Charge Controller
+    # ccCVCCSVSC = ReadChargeController()
+    # ccCVoltage = ccCVCCSVSC[0]
+    # ccCCurrent = ccCVCCSVSC[1]
+    # ccSPVoltage = ccCVCCSVSC[2]
+    # ccSPCurrent = ccCVCCSVSC[3]
+    # gpsLatLon = ReadGPS()
+    # gpsLatitude = gpsLatLon[0]
+    # gpsLongitude = gpsLatLon[1]
 
     # Do something with given threshold values
     thresholdBVL = float(thresholdBattVoltageLower)
@@ -109,6 +153,16 @@ def ReadFromSensors(thresholdBattVoltageLower=None, thresholdBattVoltageUpper=No
                 serverConfirmation = requests.get("https://remote-ecs.000webhostapp.com/index_files/pinotification.php", params=pipayload)
                 # print(serverConfirmation.text.strip())
                 pipayload.pop("noti")
+            if temperatureValueI <= thresholdTL or temperatureValueI >= thresholdTU:
+                pipayload["noti"] = "temperatureI"
+                serverConfirmation = requests.get("https://remote-ecs.000webhostapp.com/index_files/pinotification.php", params=pipayload)
+                # print(serverConfirmation.text.strip())
+                pipayload.pop("noti")
+            if temperatureValueO <= thresholdTL or temperatureValueO >= thresholdTU: 
+                pipayload["noti"] = "temperatureO"
+                serverConfirmation = requests.get("https://remote-ecs.000webhostapp.com/index_files/pinotification.php", params=pipayload)
+                # print(serverConfirmation.text.strip())
+                pipayload.pop("noti")
     except: pass  # Unable to connect to internet, so just disregard sending a notification
 
     # Fill tempDictionary with recorded values
@@ -116,7 +170,12 @@ def ReadFromSensors(thresholdBattVoltageLower=None, thresholdBattVoltageUpper=No
     tempDictionary["BatteryCurrent"] = batteryCurrentRead
     tempDictionary["SolarPanelVoltage"] = solarPanelActualVoltage
     tempDictionary["SolarPanelCurrent"] = solarPanelCurrentRead
-    tempDictionary["temperature"] = temperatureValue
+    tempDictionary["temperatureInner"] = temperatureValueI
+    tempDictionary["temperatureOuter"] = temperatureValueO
+    # tempDictionary["gpsLatitude"] = gpsLatitude
+    # tempDictionary["gpsLongitude"] = gpsLongitude
+    # tempDictionary["ccVoltage"] = ccVoltage
+    # tempDictionary["ccCurrent"] = ccCurrent
 
     # Perform Operations with ESSO
     # Solar Panel Operations
@@ -136,14 +195,14 @@ def ReadFromSensors(thresholdBattVoltageLower=None, thresholdBattVoltageUpper=No
 
     # Exhaust Operations
     if thresholdExhaustToggle == None:
-        if temperatureValue >= thresholdTU:  # For Hot Air -> Cold Air
+        if temperatureValueI >= thresholdTU:  # For Hot Air -> Cold Air
             if batteryVoltageActualVoltage >= thresholdBVL:
                 tempDictionary["exhaust"] = "on"
                 # Code to power on exhaust
             else:
                 tempDictionary["exhaust"] = "off"
                 # Code to power off exhaust
-        elif temperatureValue <= thresholdTU:  # For Cold Air -> Hot Air
+        elif temperatureValueI <= thresholdTU:  # For Cold Air -> Hot Air
             pass  # Do Nothing - Would require turning on the exhaust and changing AC to provide warmer air
     elif thresholdSolarPanelToggle == "ON":
         tempDictionary["exhaust"] = "on"
